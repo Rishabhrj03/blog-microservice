a093d7aba92df6c1e21b7c6cf3d7754aea661263
ysoni0303 <yash.chapter247@gmail.com>
2021-11-20 21:02:04 +0530

    event store in local variable

--- query/index.js
+++ query/index.js
@@ -2,6 +2,7 @@ const express = require('express');
 const cors = require('cors');
 const bodyParser = require('body-parser');
 const { response } = require('express');
+const axios = require('axios');
 const app = express();
 app.use(bodyParser.json());
 app.use(cors());
@@ -10,9 +11,7 @@ let posts = {};
 app.get('/posts', (req, res) => {
   res.send(posts);
 });
-
-app.post('/events', (req, res) => {
-  const { type, data } = req.body;
+handleEvent = (type, data) => {
   if (type === 'PostCreated') {
     const { id, title } = data;
     posts[id] = {
@@ -37,9 +36,18 @@ app.post('/events', (req, res) => {
     comment.status = status;
     comment.content = content;
   }
+};
+app.post('/events', (req, res) => {
+  const { type, data } = req.body;
+  handleEvent(type, data);
   res.send({});
 });
 
-app.listen(4002, () => {
+app.listen(4002, async () => {
   console.log('Listen on 4002');
+  const res = await axios.get('http://localhost:4005/events');
+  for (let event of res.data) {
+    console.log('Process event', event.type);
+    handleEvent(event.type, event.data);
+  }
 });
