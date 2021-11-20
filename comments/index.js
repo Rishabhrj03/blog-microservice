bae4f849f1360247e417b18ea1d22659a466bac6
ysoni0303 <yash.chapter247@gmail.com>
2021-11-20 16:59:23 +0530

    push after microservices created

--- comments/index.js
+++ comments/index.js
@@ -2,6 +2,8 @@ const express = require('express');
 const bodyParser = require('body-parser');
 const { randomBytes } = require('crypto');
 const cors = require('cors');
+const axios = require('axios');
+
 const app = express();
 app.use(bodyParser.json());
 app.use(cors());
@@ -9,14 +11,27 @@ const commentsByPostId = {};
 app.get('/posts/:id/comments', (req, res) => {
   res.send(commentsByPostId[req.params.id] || []);
 });
-app.post('/posts/:id/comments', (req, res) => {
+app.post('/posts/:id/comments', async (req, res) => {
   const commentId = randomBytes(4).toString('hex');
   console.log('req --------> ', req.body);
   const { content } = req.body;
   const comments = commentsByPostId[req.params.id] || [];
   comments.push({ id: commentId, content });
+  await axios.post('http://localhost:4005/events', {
+    type: 'CommentCreated',
+    data: {
+      id: commentId,
+      content,
+      postId: req.params.id,
+    },
+  });
   commentsByPostId[req.params.id] = comments;
   res.status(201).send(comments);
+});
+
+app.post('/events', (req, res) => {
+  console.log('Event Recieved :', req.body.type);
+  res.send({});
 });
 app.listen(4001, () => {
   console.log('listen on 4001');
